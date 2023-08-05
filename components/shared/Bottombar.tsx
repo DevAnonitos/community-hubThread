bd2fa330a655f40b58a88ac94fd3f3688af5ef43
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-05 23:23:44 +0700

    update-Add->Bottombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 
 const BottomBar = () => {
+
+    const pathname: string | undefined  = usePathname();
+
     return (
-        <div>BottomBar</div>
+        <>
+            <section className='bottombar'>
+                <div className='bottombar_container'>
+                    Link
+                </div>
+            </section>
+        </>
     );
 };
 
