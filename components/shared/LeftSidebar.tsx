bcff71b974254435766a104ed410defaa73d32b7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-05 22:50:37 +0700

    update-Add->LinkButton

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { sidebarLinks } from '@/constants';
 import { usePathname, useRouter } from "next/navigation";
 import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 
@@ -15,8 +16,67 @@ const LeftSidebar = () => {
 
     return (
         <>
-            <section className='custom-scrollbar leftsidebar'>
+            <section className='custom-scrollbar leftsidebar border-r-2 border-gray-500'>
+                <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+                    {sidebarLinks.map((link) => {
+                        const isActive =
+                            (pathName.includes(link.route) && link.route.length > 1)
+                            || pathName === link.route;
 
+                        if(link.route === '/profile') {
+                            link.route = `${link.route}/${userId}`
+                        }
+
+                        return (
+                            <>
+                                <Link
+                                    href={link.route}
+                                    key={link.label}
+                                    className={`leftsidebar_link hover:bg-primary-500 transition-all duration-500 ease-in-out delay-100
+                                        ${isActive && "bg-primary-500 "}
+                                    `}
+                                >
+                                    <Image
+                                        src={link.imgURL}
+                                        alt={link.label}
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <p className='text-light-1 max-lg:hidden'>
+                                        {link.label}
+                                    </p>
+                                </Link>
+                            </>
+                        )
+                    })}
+                </div>
+
+                <div className='mt-10 px-6'>
+                    <SignedIn>
+                        <SignOutButton
+                            signOutCallback={() => router.push("/sign-in")}
+                        >
+                            <div
+                                className='flex cursor-pointer
+                                gap-4 p-4 border-2 rounded-xl'
+                            >
+                                <Image
+                                    src='/assets/logout.svg'
+                                    alt='logout'
+                                    width={24}
+                                    height={24}
+                                />
+
+                                <p
+                                    className='text-red-500 text-xl
+                                    font-semibold max-lg:hidden'
+                                >
+                                    LogoutApp
+                                </p>
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
             </section>
         </>
     );
