2187e15332b2ce2f58c2b47033dcdf3cc6d0d902
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-06 17:16:53 +0700

    update:-Add->CreateThreadPage

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div className='text-white text-xl'>
+            CreateThreads page
+        </div>
+    );
+};
+
+export default Page;
