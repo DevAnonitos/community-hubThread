03a1d5b81cd6538a9b2c33c62853956a8be79c6a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-06 14:55:47 +0700

    update-add->ListRoute

--- constants/index.ts
+++ constants/index.ts
@@ -25,6 +25,11 @@ export const sidebarLinks = [
         label: "Communities",
     },
     {
+        imgURL: "/assets/list.svg",
+        route: "/courses",
+        label: "Courses",
+    },
+    {
         imgURL: "/assets/user.svg",
         route: "/profile",
         label: "Profile",
