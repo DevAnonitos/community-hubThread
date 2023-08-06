632f7e97a3dc170468df7485d7a9cfa4ee51960d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-06 22:55:50 +0700

    update:-Add->ProfilePage

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>IdProfilePage</div>
+    )
+}
+
+export default Page
---
+++ app/(root)/profile/edit/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>EditPage</div>
+    );
+};
+
+export default Page;
