352e31d0c5b377ff2abec51ce1f673afc0b0d1d0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-07 21:14:58 +0700

    update:Add->FormComponents

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Page = () => {
     return (
         <div className='text-white text-xl'>
-            Community page 
+            Community page
         </div>
     );
 };
