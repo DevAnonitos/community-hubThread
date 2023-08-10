c2606199096f4bf41cb892c5382b2e4552446567
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-11 00:06:35 +0700

    update:Add->ConfigSetupServer

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+
+import { Server } from 'socket.io'
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:3001`,
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+})
+
+server.listen(3001, () => {
+  console.log('✔️ Server listening on port 3001')
+})
