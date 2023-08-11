c5a513de990703de75bfd93dda0e0c92f9321cce
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-11 19:58:57 +0700

    update:Add->RemovePackSocketIo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,24 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 import { Pagination } from "@/components/shared";
 
 export default async function Home() {
   return (
     <>
-      <h1 className="head-text text-left">
-        Home
-      </h1>
+      <div className="flex items-center">
+          <Image
+              src="/assets/home.svg"
+              alt='UserProfile'
+              width={34}
+              height={34}
+              className='object-contain flex items-center mb-10 mr-4'
+          />
+        <h1 className="head-text text-left mb-10">
+          Home
+        </h1>
+      </div>
 
       <section className="mt-9 flex flex-col gap-10 text-white">
 
