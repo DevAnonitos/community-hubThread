af9eb5ce6707ca243230a122e00d5c6c3a75e8d8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-11 17:46:41 +0700

    update:Add->RouteServer

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
 
-import { Server } from 'socket.io'
+import { Server } from 'socket.io';
 const io = new Server(server, {
   cors: {
-    origin: `http://localhost:3001`,
+    origin: `*`,
   },
-})
+});
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 
+  socket.on('input-change', msg => {
+    socket.broadcast.emit('update-input', msg);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
-})
+});
 
 server.listen(3001, () => {
   console.log('✔️ Server listening on port 3001')
-})
+});
