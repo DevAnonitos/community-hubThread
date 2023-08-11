6214a8bcc7c49709fcbdc9082d2f013f9446b0d6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-11 19:35:00 +0700

    update:Add->HomePage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,23 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { Pagination } from "@/components/shared";
 
 export default async function Home() {
   return (
-    <main className="text-white">
-      Thread
-    </main>
+    <>
+      <h1 className="head-text text-left">
+        Home
+      </h1>
+
+      <section className="mt-9 flex flex-col gap-10 text-white">
+
+      </section>
+
+      <Pagination
+        path="/"
+        pageNumber={3}
+      />
+    </>
   );
 };
