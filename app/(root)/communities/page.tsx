01d41434712ee6c8ce78c759b73b1313b5217ebe
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-15 23:12:02 +0700

    update:Add->Communitypage

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import Image from 'next/image';
 
-const Page = () => {
+import { SearchBar, Pagination } from '@/components/shared';
+import CommunityCard from '@/components/cards/CommunityCard';
+
+
+
+const Page = async (
+    {
+        searchParams,
+    }:
+    {
+        searchParams: {
+            [key: string]: string | undefined,
+        }
+
+    }) => {
+
+    const user = await currentUser();
+
     return (
-        <div className='text-white text-xl'>
-            Community page
-        </div>
+        <>
+            <section>
+                <div className='flex items-center'>
+
+                    <Image
+                        src="/assets/community.svg"
+                        alt='UserProfile'
+                        width={34}
+                        height={34}
+                        className='object-contain flex items-center mb-10 mr-4'
+                    />
+
+                    <h1 className='head-text mb-10'>
+                        Communities
+                    </h1>
+                </div>
+
+                <div className='mt-5'>
+                    <SearchBar routeType='communities' />
+                </div>
+
+                <section className='mt-9 flex flex-wrap gap-4'>
+
+                </section>
+
+                <Pagination
+                    path='communities'
+                    pageNumber={5}
+                />
+            </section>
+        </>
     );
 };
 
