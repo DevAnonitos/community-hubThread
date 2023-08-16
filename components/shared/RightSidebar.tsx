7872ae6f62f1cfe76e829b96ae7690a1dba16b59
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-16 22:26:49 +0700

    update:Add->FixError

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import { currentUser } from "@clerk/nextjs";
-import UserCard from '../cards/UserCard';
 import { Skeleton } from '../ui/skeleton';
+import UserCard from '../cards/UserCard';
 
 const RightSidebar = async () => {
 
