98daddaf0189ac6872927dc63e5b01df61ca285e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-16 23:05:51 +0700

    update:Add->SetUpConnectDB

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDb = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(!process.env.MONGODB_URL) {
+        return console.log("Missing MongoDB URL");
+    }
+
+    if(isConnected) {
+        console.log("MongoDB connection already established");
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+        console.log("MongoDB connected");
+    } catch (error: any) {
+        console.log(error);
+    }
+};
