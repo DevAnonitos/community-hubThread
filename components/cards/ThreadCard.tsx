2e75e091586bc35830b4e8dc85c60c068d2d3849
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-20 10:58:24 +0700

    update:Add->ThreadCard

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+    
+};
+
+const ThreadCard = ({
+
+}: Props) => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default ThreadCard;
