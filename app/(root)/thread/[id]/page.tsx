b99e46997babcb9848e6db234a131f62e81f46d9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-08-25 21:49:32 +0700

    update:Add->ThreadPage

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { redirect } from 'next/navigation';
+import { currentUser } from '@clerk/nextjs';
 
-const Page = () => {
+import Comment from '@/components/forms/Comment';
+import ThreadCard from '@/components/cards/ThreadCard';
+
+import { fetchUser } from '@/lib/actions/user.actions';
+
+export const revalidate = 0;
+
+const Page = async ({ params }: { params: { id: string } }) => {
+
+    if(!params.id) return null;
+
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarding) return redirect("/onboarding");
+
     return (
-        <div>ThreadPage</div>
+        <>
+            <Suspense>
+                <section className='relative'>
+                    <div>
+                        <ThreadCard
+
+                        />
+                    </div>
+
+                    <div className='mt-7'>
+                        <Comment />
+                    </div>
+
+                    <div className='mt-10'>
+                        <ThreadCard
+                            
+                        />
+                    </div>
+                </section>
+            </Suspense>
+        </>
     );
 };
 
