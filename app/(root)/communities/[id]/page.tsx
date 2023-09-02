7f65e9d4e8f72f123dba9e7dd0b83826602b827c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-02 20:59:38 +0700

    ưpdate:Add->CommunityPageId

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -1,14 +1,11 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, Fragment } from 'react';
 import Image from "next/image";
 
 import { currentUser } from '@clerk/nextjs';
 import { communityTabs } from '@/constants';
 
-import {
-    UserCard,
-    ThreadCard,
-    CommunityCard
-} from '@/components/cards';
+import { UserCard } from '@/components/cards';
+import { ThreadsTab, ProfileHeader } from '@/components/shared';
 
 import {
     TabsContent,
@@ -17,11 +14,69 @@ import {
     TabsTrigger,
 } from '@/components/ui/tabs';
 
-const Page = ({ params }: { params: {id: string} }) => {
+const Page = async({ params }: { params: {id: string} }) => {
+
+    const user = await currentUser();
+    if(!user) return null;
+
     return (
         <>
             <Suspense>
-                Community page id
+                <section>
+
+                    <div className='mt-9'>
+                        <Tabs defaultValue='threads' className='w-full'>
+                            <TabsList className='tab'>
+                                {communityTabs.map((tab) => (
+                                    <>
+                                        <Suspense>
+                                            <Fragment key={tab.label}>
+                                                <TabsTrigger value={tab.value} className='tab'>
+                                                    <Image
+                                                        src={tab.icon}
+                                                        alt={tab.label}
+                                                        width={24}
+                                                        height={24}
+                                                        className='object-contain'
+                                                    />
+                                                    <p className='max-sm:hidden'>
+                                                        {tab.label}
+                                                    </p>
+                                                </TabsTrigger>
+                                            </Fragment>
+                                        </Suspense>
+                                    </>
+                                ))}
+                            </TabsList>
+
+                            <TabsContent
+                                value='threads'
+                                className='w-full text-light-1'
+                            >
+                                {/* @ts-ignore */}
+
+                            </TabsContent>
+
+                            <TabsContent
+                                value='members'
+                                className='mt-9 w-full text-light-1'
+                            >
+                                <section className='mt-9 flex flex-col gap-10'>
+                                    {}
+                                </section>
+                            </TabsContent>
+
+                            <TabsContent value='requests' className='w-full text-light-1'>
+                                {/* @ts-ignore */}
+                                <ThreadsTab
+                                    currentUserId={user.id}
+                                    accountId=''
+                                    accountType='Community'
+                                />
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+                </section>
             </Suspense>
         </>
     );
