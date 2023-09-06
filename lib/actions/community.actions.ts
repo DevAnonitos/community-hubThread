f38ee8437dba697b0bf0302a18e2235e0f71b386
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-06 10:00:50 +0700

    update:Add->createCommunity-Fn-API

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -1,9 +1,47 @@
 "use server";
-// Use Server file 
+// Use Server file
 import { FilterQuery, SortOrder } from "mongoose";
 import { Community, Thread, User } from "../models";
 import { connectToDB } from "../mongoose";
 
 // Handle File Func
+export const createCommunity = async(
+    id: string,
+    name: string,
+    username: string,
+    image: string,
+    bio: string,
+    createdById: string,
+) => {
+    try {
+        connectToDB();
 
+        const user = await User.findOne({
+            id: createdById,
+        });
+
+        if(!user) {
+            throw new Error("User not found");
+        }
+
+        const newCommunity = new Community({
+            id,
+            name,
+            username,
+            image,
+            bio,
+            createdBy: user._id,
+        });
+
+        const createdCommunity = await newCommunity.save();
+
+        user.communities.push(createdCommunity._id);
+        await user.save();
+
+        return createCommunity;
+    } catch (error: any) {
+        console.error("Error creating community: ", error);
+        throw error;
+    }
+};
 
