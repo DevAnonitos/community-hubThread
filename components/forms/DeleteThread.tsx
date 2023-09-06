2cbadd23847b1d6f5fdf77eebff6f2359e2f4ca7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-06 08:56:59 +0700

    update:Add->AlertDialog-To-DeleteConfirm

--- components/forms/DeleteThread.tsx
+++ components/forms/DeleteThread.tsx
@@ -9,6 +9,18 @@ import { deleteThread } from '@/lib/actions/thread.actions';
 import { ToastAction } from '../ui/toast';
 import { useToast } from '../ui/use-toast';
 
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '../ui/alert-dialog';
+
 interface Props {
     threadId: string;
     currentUserId: string;
@@ -32,27 +44,49 @@ const DeleteThread = ({
 
     if(currentUserId !== authorId || pathName === "/") return null;
 
+    const handleDeleteThread = async () => {
+        await deleteThread(JSON.parse(threadId), pathName);
+            if(!parentId || !isComment){
+                router.push("/");
+                toast({
+                    variant: "default",
+                    title: "Delete Threads is successful",
+                    description: "See your threads in homepage",
+                    action: <ToastAction altText="Continue">Continue</ToastAction>,
+                });
+            }
+    };
+
     return (
-        <div className='bg-gray-700 p-2 rounded-full'>
-            <Image
-                src="/assets/delete.svg"
-                alt='Delete'
-                width={20}
-                height={20}
-                className='cursor-pointer object-contain'
-                onClick={async () => {
-                    await deleteThread(JSON.parse(threadId), pathName);
-                    if(!parentId || !isComment){
-                        router.push("/");
-                        toast({
-                            variant: "default",
-                            title: "Delete Threads is successful",
-                            description: "See your threads in homepage",
-                            action: <ToastAction altText="Continue">Continue</ToastAction>,
-                        });
-                    }
-                }}
-            />
+        <div className='flex items-center justify-center bg-gray-700 p-2 rounded-full'>
+            <AlertDialog>
+                <AlertDialogTrigger>
+                    <Image
+                        src="/assets/delete.svg"
+                        alt='Delete'
+                        width={20}
+                        height={20}
+                        className='cursor-pointer object-contain'
+                    />
+                </AlertDialogTrigger>
+                <AlertDialogContent className='bg-dark-2 border-[1px] border-gray-700'>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle className='text-red-600 text-lg'>Are you sure delete Thread?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel className='text-red-600'>
+                            Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction onClick={handleDeleteThread} className='bg-green-600 hover:bg-green-500'>
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </div>
     );
 };
