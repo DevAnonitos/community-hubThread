956e7c8f83c4c8b522118131cfa3f217b55a4095
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-07 15:40:51 +0700

    update:Add->ConfigServer

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
-import { Server } from 'socket.io';
-
-const io = new Server(server, {
-  cors: {
-    origin: `*`,
-  },
-});
+// Your existing server logic
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  socket.on('input-change', msg => {
-    socket.broadcast.emit('update-input', msg);
-  });
+  // Emit activity updates
+  const activityUpdate = {
+    /* Your activity data */
+  };
+  socket.emit('activity', activityUpdate);
 
+  // Handle client disconnection
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
 
 server.listen(3001, () => {
-  console.log('✔️ Server listening on port 3001')
+  console.log('Server is running on port 3001');
 });
