b2bb16d41d0e30502f925046a4a9fd305c994681
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-19 15:09:51 +0700

    update:Add->CourseUserSchema

---
+++ lib/models/course.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    authorCourse: {
+        type: String,
+        required: true,
+    },
+    typeCourse: {
+        type: String,
+        required: true,
+    },
+    imageCourse: String,
+    description: String,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Course = mongoose.models.Course || mongoose.model("Course", courseSchema);
+
+export default Course;
--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,9 +1,11 @@
 import User from "./user.model";
 import Thread from "./thread.model";
 import Community from "./community.model";
+import Course from "./course.model";
 
 export {
     User,
     Thread,
     Community,
+    Course,
 };
