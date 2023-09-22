41a105582a31d90cac8f6852e8f4559839100dd9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-22 12:35:51 +0700

    update:Add->FixError

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/", "/api/webhook/clerk"],
 
 
   ignoredRoutes: ["/api/webhook/clerk"],
