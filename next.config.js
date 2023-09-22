0def1ece39c82f7b0b2d5385f8b3900a9479226a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-22 22:33:12 +0700

    update:Add->ConfigNextjs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         serverActions: true,
         serverComponentsExternalPackages: ["mongoose"],
         webVitalsAttribution: ['CLS', 'LCP'],
+        incrementalCacheHandlerPath: require.resolve('./cache-handler.js'),
     },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
@@ -36,6 +37,12 @@ const nextConfig = {
     ],
     productionBrowserSourceMaps: true,
     trailingSlash: true,
+    onDemandEntries: {
+        // period (in ms) where the server will keep pages in the buffer
+        maxInactiveAge: 25 * 1000,
+        // number of pages that should be kept simultaneously without being disposed
+        pagesBufferLength: 2,
+    },
 };
 
 module.exports = nextConfig;
