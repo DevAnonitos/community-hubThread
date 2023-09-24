a2e105a7d729afa34b926a620bec654da4ea3c0a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-24 22:20:10 +0700

    update:Add->DeleteAPIHub

--- app/(root)/news/page.tsx
+++ app/(root)/news/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
-import { getNews } from '@/lib/services/getNews';
+import React from 'react';
 
 const Page = () => {
-    const [coins, setCoins] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const result = await getNews();
-                if (result && result.data && Array.isArray(result.data.coins)) {
-                    setCoins(result.data.coins);
-                }
-            } catch (error) {
-                console.error('Error fetching news:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
+    
 
     return (
         <div className='text-white'>
