0aa22375693f15cbd3e9c6be20d3d230e537bbb8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-24 22:04:55 +0700

    update:Add->GetNewsAPI

--- app/(root)/news/page.tsx
+++ app/(root)/news/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+"use client";
 
-const page = () => {
+import React, { useEffect, useState } from 'react';
+import { getNews } from '@/lib/services/getNews';
+
+const Page = () => {
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await getNews();
+                if (result && result.data && Array.isArray(result.data.coins)) {
+                    setCoins(result.data.coins);
+                }
+            } catch (error) {
+                console.error('Error fetching news:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div>page news</div>
+        <div className='text-white'>
+            page news
+            <div className='text-white'>
+                
+            </div>
+        </div>
     );
 };
 
-export default page;
+export default Page;
