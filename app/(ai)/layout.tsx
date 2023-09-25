62e6e5fd7c7bb8a1fb9ed628c4a85018654cd0d8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-25 16:56:18 +0700

    update:Add->ChatAI-PageLayout

---
+++ app/(ai)/layout.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { dark } from "@clerk/themes"
+
+// Styles
+import "../globals.css";
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: {
+        default: 'Next.js AI Chatbot',
+        template: `%s - Next.js AI Chatbot`
+    },
+    description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+    themeColor: [
+        { media: '(prefers-color-scheme: light)', color: 'white' },
+        { media: '(prefers-color-scheme: dark)', color: 'black' }
+    ],
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon-16x16.png',
+        apple: '/apple-touch-icon.png'
+    }
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode,
+}) {
+
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={`${inter.className}
+                        bg-dark-1`
+                    }
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+};
