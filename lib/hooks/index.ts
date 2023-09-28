884ed80e1acb01364eaa3709134a79a0bafbeb77
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-28 23:42:01 +0700

    update:Add->Hook-useEnterSubmit

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,9 +1,11 @@
 import useCopyToClipboard from "./use-copy-to-clipboard";
 import useBottom from "./use-at-bottom";
 import useLocalStorage from "./use-local-storage";
+import useEnterSubmit from "./use-enter-submit";
 
 export {
     useBottom,
     useCopyToClipboard,
     useLocalStorage,
+    useEnterSubmit,
 };
