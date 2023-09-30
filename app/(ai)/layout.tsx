cd2fbc0c049cb2c302be54831cfc352d400e0768
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-30 22:46:42 +0700

    update:Add->FixError

--- app/(ai)/layout.tsx
+++ app/(ai)/layout.tsx
@@ -1,54 +1,54 @@
-// import React from "react";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import type { Metadata } from 'next';
-// import { Inter } from 'next/font/google';
-// import { dark } from "@clerk/themes";
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { dark } from "@clerk/themes";
 
-// import { TopBar, Providers } from "@/components/shared";
+import { TopBar, Providers } from "@/components/shared";
 
-// // Styles
-// import "../globals.css";
-// const inter = Inter({ subsets: ['latin'] });
+// Styles
+import "../globals.css";
+const inter = Inter({ subsets: ['latin'] });
 
-// export const metadata: Metadata = {
-//     title: {
-//         default: 'Next.js AI Chatbot',
-//         template: `%s - Next.js AI Chatbot`
-//     },
-//     description: 'An AI-powered chatbot template built with Next.js and Vercel.',
-//     themeColor: [
-//         { media: '(prefers-color-scheme: light)', color: 'white' },
-//         { media: '(prefers-color-scheme: dark)', color: 'black' }
-//     ],
-//     icons: {
-//         icon: '/favicon.ico',
-//         shortcut: '/favicon-16x16.png',
-//         apple: '/apple-touch-icon.png'
-//     }
-// };
+export const metadata: Metadata = {
+    title: {
+        default: 'Next.js AI Chatbot',
+        template: `%s - Next.js AI Chatbot`
+    },
+    description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+    themeColor: [
+        { media: '(prefers-color-scheme: light)', color: 'white' },
+        { media: '(prefers-color-scheme: dark)', color: 'black' }
+    ],
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon-16x16.png',
+        apple: '/apple-touch-icon.png'
+    }
+};
 
-// export default function RootLayout({
-//     children,
-// }: {
-//     children: React.ReactNode,
-// }) {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode,
+}) {
 
-//     return (
-//         <ClerkProvider
-//             appearance={{
-//                 baseTheme: dark,
-//             }}
-//         >
-//             <html lang="en">
-//                 <body
-//                     className={`${inter.className}
-//                         bg-dark-1`
-//                     }
-//                 >
-//                     <TopBar />
-//                     {children}
-//                 </body>
-//             </html>
-//         </ClerkProvider>
-//     );
-// };
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={`${inter.className}
+                        bg-dark-1`
+                    }
+                >
+                    <TopBar />
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+};
