d7e6d09467b955cafd9c914cf5fff72703afb1c3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-30 11:13:42 +0700

    update:Add->ExternalLink-Components

---
+++ components/customs/index.ts
@@ -0,0 +1,11 @@
+import ClearHistory from "./ClearHistory";
+import MemoizedReactMarkdown from "./MarkDown";
+import ThemeToggle from "./ThemeToggle";
+import ExternalLink from "./ExternalLink";
+
+export {
+    ClearHistory,
+    MemoizedReactMarkdown,
+    ThemeToggle,
+    ExternalLink,
+};
