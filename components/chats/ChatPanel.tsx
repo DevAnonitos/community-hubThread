4b9794dd880c0272e9f9673aaefdd4646241bac5
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-30 17:20:41 +0700

    update:Add->ChatPanel

---
+++ components/chats/ChatPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { type UseChatHelpers } from 'ai/react';
+import { Button } from '../ui/button';
+
+const ChatPanel = () => {
+    return (
+        <div>ChatPanel</div>
+    );
+};
+
+export default ChatPanel;
