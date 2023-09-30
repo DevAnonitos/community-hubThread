49895ee9f455a7f41c5dc16a68f1569b76828025
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-09-30 21:39:08 +0700

    update:Add->ConfigClerk-NextJs

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
